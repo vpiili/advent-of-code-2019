646a979f9f621b8b089bd7965d7874fb83f0368d
Ville Piili <viakpi@utu.fi>
2019-12-01 19:28:47 +0200

    Changed so that day1 shows answer to part1 also

--- src/day1.groovy
+++ src/day1.groovy
@@ -3,19 +3,26 @@
  * Ville Piili
  **/
 
-//assert calculateFuel('12') == 2
+assert calculateFuel(12) == 2
 assert calculateFuel(14) == 2
-assert calculateFuel(100756) == 50346
-assert calculateFuel(1969) == 966
+assert calculateFuel(1969)
+assert calculateFuel(100756)
 
-def file = new File('../input/day1.txt')
-println fuelCounterUpper(file.readLines())
+assert calculateFuel(14, true) == 2
+assert calculateFuel(1969, true) == 966
+assert calculateFuel(100756, true) == 50346
 
-def fuelCounterUpper(List input) {
-    return input.inject(0) { acc, line -> acc += calculateFuel(Integer.parseInt(line)) }
+
+def input = new File('../input/day1.txt').readLines()
+println "Part 1 --  ${fuelCounterUpper(input)}"
+println "Part 2 --  ${fuelCounterUpper(input, true)}"
+
+def fuelCounterUpper(List input, part2 = false) {
+    return input.inject(0) { acc, line -> acc += calculateFuel(Integer.parseInt(line), part2) }
 }
 
-def calculateFuel(int input, int totalFuel = 0) {
+def calculateFuel(int input, part2 = false, int totalFuel = 0) {
     def fuel = ((input / 3) as int) - 2
-    return fuel > 0 ? calculateFuel(fuel, totalFuel + fuel) : totalFuel
+    if (!part2) return fuel
+    return fuel > 0 ? calculateFuel(fuel, part2, totalFuel + fuel) : totalFuel
 }
